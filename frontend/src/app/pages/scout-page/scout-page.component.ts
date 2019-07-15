1f6a36157c2a6637ae50af731d46a58ac581f0ba
SirCremefresh <donato.wolfisberg@gmail.com>
2019-07-15 18:18:30 +0200

    added date picker

--- frontend/src/app/pages/scout-page/scout-page.component.ts
+++ frontend/src/app/pages/scout-page/scout-page.component.ts
@@ -1,21 +1,25 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Observable, Subscription} from 'rxjs';
 import {Child} from '../../models/child.model';
 import {Scout} from '../../models/scout.model';
 import {ChildService} from '../../services/child.service';
 import {ScoutService} from '../../services/scout.service';
+import bulmaCalendar from 'bulma-calendar/dist/js/bulma-calendar';
 
 @Component({
   selector: 'app-scout-page',
   templateUrl: './scout-page.component.html',
   styleUrls: ['./scout-page.component.scss']
 })
-export class ScoutPageComponent implements OnInit, OnDestroy {
+export class ScoutPageComponent implements OnInit, AfterViewInit, OnDestroy {
   public children$: Observable<Child[]>;
   public scout$: Observable<Scout>;
+  public calendar: bulmaCalendar;
   private scoutId: string = undefined;
   private params$$: Subscription;
+
+  @ViewChild('dateInput', {static: false}) dateInput: ElementRef<HTMLInputElement>;
 
   constructor(
     private route: ActivatedRoute,
@@ -37,5 +41,14 @@ export class ScoutPageComponent implements OnInit, OnDestroy {
 
   createChild(child: { name: string }) {
     this.childService.createChild(child, this.scoutId);
+  }
+
+  ngAfterViewInit(): void {
+    this.calendar = bulmaCalendar.attach('[type="date"]', {
+      type: 'date',
+      dateFormat: 'DD/MM/YYYY',
+      startDate: new Date(),
+      showClearButton: false
+    })[0];
   }
 }
